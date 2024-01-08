b93c7479c606abfac1f6e818a0fca5da3bf92563
krml19 <marcin@apptailors.co>
2024-01-09 00:39:07 +0100

    fix

--- lib/tolgee/translation_widget.dart
+++ lib/tolgee/translation_widget.dart
@@ -21,24 +21,24 @@ class TranslationWidget extends StatefulWidget {
 class _TranslationWidgetState extends State<TranslationWidget> {
   @override
   Widget build(BuildContext context) {
-    final isTranslationEnabled = TolgeeSdk.instance.isTranslationEnabled;
-    final backgroundColor = isTranslationEnabled ? Colors.red : null;
-
-    final onTap = isTranslationEnabled
-        ? () {
-            setState(() {
-              TolgeeSdk.instance.toggleTranslationEnabled();
-            });
-          }
-        : () {
-            setState(() {
-              TolgeeSdk.instance.toggleTranslationEnabled();
-            });
-          };
-
     return ListenableBuilder(
         listenable: TolgeeSdk.instance,
         builder: (BuildContext context, Widget? child) {
+          final isTranslationEnabled = TolgeeSdk.instance.isTranslationEnabled;
+          final backgroundColor = isTranslationEnabled ? Colors.green : null;
+
+          final onTap = isTranslationEnabled
+              ? () {
+                  setState(() {
+                    TolgeeSdk.instance.toggleTranslationEnabled();
+                  });
+                }
+              : () {
+                  setState(() {
+                    TolgeeSdk.instance.toggleTranslationEnabled();
+                  });
+                };
+
           return GestureDetector(
             onTap: onTap,
             child: Container(
